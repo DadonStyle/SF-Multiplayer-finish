92480744d6739ac9b572d9d2b008ac8b553de5c0
DadonStyle <noamoni9@gmail.com>
2025-08-19 21:17:58 +0300

    remove unused game controller,

--- BE/src/game/game.module.ts
+++ BE/src/game/game.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { GameController } from './game.controller';
 import { GameService } from './game.service';
 import { GameGateway } from './game.gateway';
 import { GameStore } from './game.store';
 
 @Module({
-  controllers: [GameController],
+  controllers: [],
   providers: [GameService, GameGateway, GameStore],
   exports: [GameService, GameStore],
 })
